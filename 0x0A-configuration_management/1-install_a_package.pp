7fa18ff7f297e255a946dd594658eb84f0f2bf19
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-04-19 14:02:31 -0500

    sad

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,7 +1,6 @@
 # Instalo un paquete mas no si sirve.
 
-package { 'puppet-lint':
-    ensure   => '2.5.0',
-    provider => gem,
-    source   =>  'https://rubygems.org/gems/puppet-lint/versions/2.5.0',
+package { 'flask':
+    ensure   => '2.1.0',
+    provider => pip3,
 }
