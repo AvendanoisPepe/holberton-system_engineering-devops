506274f61177f179d8698eca07cf470bac9310ce
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-19 07:54:15 -0500

    correcciones

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -20,7 +20,7 @@ if __name__ == "__main__":
             total += 1
             if tareas.get('completed'):
                 completed += 1
-    print("Employee {} is done with tasks({}/{})"
+    print("Employee {} is done with tasks({}/{}):"
           .format(nombre, completed, total))
     print("\n".join(["\t " + tareas.get('title') for tareas in todos
           if tareas.get('userId') == int(did) and tareas.get('completed')]))
