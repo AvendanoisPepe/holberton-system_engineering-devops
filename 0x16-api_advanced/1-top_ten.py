396a236a77d901b918b05662b22252420e753db8
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-21 00:27:34 -0500

    pepe

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -4,13 +4,25 @@ las primeras 10 publicaciones populares enumeradas para un subreddit
 determinado.
 """
 
+
 def top_ten(subreddit):
     """Solicitud de reddit api y respuesta del proceso"""
     import requests
 
-    url = requests.get("https://www.reddit.com/r/{}/top.json?limit=10".format(subreddit), headers={"Content-Type": "application/json", "User-Agent": "platform"}, allow_redirects = False)
+    url = requests.get("https://www.reddit.com/r/{}/top.json?limit=10"
+                       .format(subreddit),
+                       headers={"Content-Type": "application/json",
+                                "User-Agent": "platform"},
+                       allow_redirects=False)
+
+    url = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit),
+                       headers={"Content-Type": "application/json",
+                                "User-Agent": "platform"},
+                       allow_redirects=False)
+
     if url.status_code == 200:
         for titulos in url.json().get("data").get("children"):
-                print(titulos.get("data").get("title"))
+            print(titulos.get("data").get("title"))
     else:
         print(None)
