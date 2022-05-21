ae53d004d0dc35345f537e4a9227418bf2ba3850
AvendanoisPepe <jsavendano62@misena.edu.co>
2022-05-21 00:22:04 -0500

    pepe

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -5,11 +5,17 @@ un subreddit determinado. Si se proporciona un subreddit no válido,
 la función debería devolver 0.
 """
 
+
 def number_of_subscribers(subreddit):
     """Solicitud de reddit api y respuesta del proceso"""
     import requests
 
-    url = requests.get("https://www.reddit.com/r/{}/about.json".format(subreddit), headers={"Content-Type": "application/json", "User-Agent": "platform"}, allow_redirects = False)
+    url = requests.get("https://www.reddit.com/r/{}/about.json"
+                       .format(subreddit),
+                       headers={"Content-Type": "application/json",
+                                "User-Agent": "platform"},
+                       allow_redirects=False)
+
     if url.status_code == 200:
         return url.json().get("data").get("subscribers")
     else:
